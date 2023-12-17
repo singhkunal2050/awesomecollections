363bda968db30371f4f79f56751ee3a5ef674b5f
Kunal Singh <singhkunal2050@gmail.com>
2023-12-17 14:23:57 +0530

    added style impro

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
             Star this on GitHub
           </a>
         </div>
-        <div className='text-center py-4'>
-          <h2 className="text-2xl font-bold my-4">
+        <div className='text-center py-4 mt-[100px]'>
+          <h2 className="text-2xl font-bold mb-4">
             <span className={`${farro.className} text-4xl`}>Awesome</span>  Collections</h2>
           <p className='max-w-[70ch] mx-auto'>Explore a vast repository of carefully selected GitHub repositories, each brimming with exceptional resources to enhance your programming skills, expand your knowledge base, and fuel your innovative projects. Whether you are a seasoned developer or just starting your coding adventure, this treasure trove of meticulously curated resources will equip you with the tools and insights you need to excel in the dynamic world of software development.</p>
         </div>
